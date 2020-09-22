22aaf6789b76e4cb6392b57398ffd2214b2443b0
Harold Andrrés Bartolo Moscoso <habartolom@gmail.com>
2020-09-21 19:24:01 -0500

    responsive

--- assets/scripts/ansalo.js
+++ assets/scripts/ansalo.js
@@ -2,51 +2,45 @@ const secuence = 'ABCDEFGHIJ1234567890';
 let lettersString = '';
 let numbersString = '';
 
-const letters = document.getElementById('letters');
-const numbers = document.getElementById('numbers');
+const lettersInput = document.getElementById('lettersInput');
+const numbersInput = document.getElementById('numbersInput');
 const keys = document.getElementsByClassName('key');
 
-for(let i = 0; i < keys.length; i++){
+for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
-    key.addEventListener('click', ()=>{
-        if(key.id == ''){
+    key.addEventListener('click', () => {
+        if (key.id == '') {
 
         }
-        else if(key.id == 'reset'){
+        else if (key.id == 'reset') {
             lettersString = '';
             numbersString = '';
-            letters.value = lettersString;
-            numbers.value = numbersString;
+            lettersInput.value = lettersString;
+            numbersInput.value = numbersString;
         }
-        else{
-            
-            const onInputs =  lettersString + numbersString;
-            if(onInputs == secuence){
-                //alert('No se pueden agregar más caracteres');
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'No se pueden agregar más caracteres'
-                  })
-            }
-            else{
+        else {
+            const inputsString = lettersString + numbersString;
+            if (inputsString == secuence)
+                showAlertSwal('error', 'Oops! Ya terminaste!', 'No se pueden agregar más caracteres');
+            else {
                 let letter = key.id[0].toUpperCase();
                 let number = key.id[2];
-    
-                let intent = onInputs + letter;
-                if(intentIsValid(intent)){
+
+                let intent = inputsString + letter;
+                if (intentIsValid(intent)) {
                     lettersString += letter;
-                    letters.value = lettersString;
+                    lettersInput.value = lettersString;
                 }
-                else{
-                    intent = onInputs + number;
-                    if(intentIsValid(intent)){
+                else {
+                    intent = inputsString + number;
+                    if (intentIsValid(intent)) {
                         numbersString += number;
-                        numbers.value = numbersString;
+                        numbersInput.value = numbersString;
+                        if ((lettersString + numbersString) == secuence)
+                            showAlertSwal('success', 'Has terminado!', '');
                     }
-                    else{
+                    else
                         findMissingLetters(letter, number);
-                    }
                 }
             }
         }
@@ -54,45 +48,43 @@ for(let i = 0; i < keys.length; i++){
     });
 }
 
-const intentIsValid = (intent)=>{
+const intentIsValid = (intent) => {
     const index = intent.length - 1;
-    if(intent[index] == secuence[index])
+    if (intent[index] == secuence[index])
         return true;
     return false;
 };
 
-const findMissingLetters = (letter, number)=>{
-    const onInputs = lettersString + numbersString;
+const findMissingLetters = (letter, number) => {
+    const inputsString = lettersString + numbersString;
     let missingLetters = '';
 
-    if(secuence.indexOf(letter) <= (onInputs.length - 1)){
-        if(secuence.indexOf(number) <= (onInputs.length - 1)){
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'No puedes ingresar este caracter'
-              })
-        }
-        else{
-            for(let i = onInputs.length; i < secuence.indexOf(number); i++){
-                missingLetters += secuence[i] + ', ';
-            }
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Faltan los caracteres ' + missingLetters + 'para poder ingresar este número'
-              })
-        }       
-    }
-    else{
-        for(let i = onInputs.length; i < secuence.indexOf(letter); i++){
+    let indexOfChar = secuence.indexOf(letter);
+
+    if (inputsString.length < indexOfChar) {
+        for (let i = inputsString.length; i < indexOfChar; i++) {
             missingLetters += secuence[i] + ', ';
         }
-        //alert('Faltan los caracteres ' + missingLetters + 'para poder ingresar esta letra');
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Faltan los caracteres ' + missingLetters + 'para poder ingresar esta letra'
-          })
+        showAlertSwal('error', 'Oops...', 'Faltan los caracteres ' + missingLetters + 'para poder ingresar esta letra');
     }
+    else {
+        indexOfChar = secuence.indexOf(number);
+        if (inputsString.length < indexOfChar) {
+            for (let i = inputsString.length; i < indexOfChar; i++) {
+                missingLetters += secuence[i] + ', ';
+            }
+            showAlertSwal('error', 'Oops...', 'Faltan los caracteres ' + missingLetters + 'para poder ingresar este número');
+        }
+        else {
+            showAlertSwal('error', 'Oops...', 'No puedes ingresar este caracter');
+        }
+    }
+};
+
+const showAlertSwal = (icon, title, text) => {
+    Swal.fire({
+        icon: icon,
+        title: title,
+        text: text
+    });
 };
